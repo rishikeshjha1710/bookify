4fd3c0df0c4bf33c7922dfff878ef4b79353c61c
rishikeshjha1710 <goldenjha2016@gmail.com>
2025-11-15 22:15:50 +0530

    Upadted with home pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react';
 import { Menu, X, BookOpen, Users, DollarSign, Briefcase, ArrowRight, Zap, Target, Brain } from 'lucide-react'; // Added new icons
 
